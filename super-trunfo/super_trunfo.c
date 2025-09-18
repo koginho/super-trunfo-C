6474d8bac73781bab3a51fef08fda264da6a6c43
koginho <mcatalanodraw@gmail.com>
2025-09-18 15:07:29 -0300

    [ADD] Lógica de comparação de cartas iniciada.

--- super-trunfo/super_trunfo.c
+++ super-trunfo/super_trunfo.c
@@ -5,9 +5,9 @@
 int main() { // Começo da execução.
 
     // Declaração das váriaveis.
-    char estado[40], cartaCodigo[5], cidadeNome[40];
-    int populacao, pontosTuristicos;
-    float areaEmKM, pib;
+    char estado[40], cartaCodigo[5], cidadeNome[40], estado_2[40], cartaCodigo_2[5], cidadeNome_2[40];
+    int populacao, pontosTuristicos, populacao_2, pontosTuristicos_2;
+    float areaEmKM, pib, areaEmKM_2, pib_2;
 
     printf("Digite as informações da carta número 1\n");
 
@@ -36,12 +36,20 @@ int main() { // Começo da execução.
     estado[strcspn(estado, "\n")] = 0;
     cidadeNome[strcspn(cidadeNome, "\n")] = 0;
 
+    // Calculando PIB per capita e Densidade Populacional
+    float pibPerCapita = (pib / populacao);
+    float pibPerCapita_2 = (pib_2 / populacao_2);
+    float densidadePopulacional = (populacao / areaEmKM);
+    float densidadePopulacional_2 = (populacao_2 / areaEmKM_2);
+
     // Primeira impressão de dados.
     printf("Informações da carta número 1:\n");
     printf("Estado: %s - Cidade: %s\n", estado, cidadeNome);
     printf("Código: %s - População: %d\n", cartaCodigo, populacao);
     printf("Área (km²): %.2f - PIB: %.2f\n", areaEmKM, pib);
     printf("Em %s tem %d pontos turísticos.\n", cidadeNome, pontosTuristicos);
+    printf("O PIB per capita: %.2f\n", pibPerCapita);
+    printf("Densidade Populacional: %.3f hab./km².\n", densidadePopulacional);
 
     // Função para pausar a execução do código.
     printf("Preciona ENTER para continuar. \n");
@@ -51,36 +59,53 @@ int main() { // Começo da execução.
     printf("Digite as informações da carta número 2\n");
 
     printf("Estado: ");
-    fgets(estado, 40, stdin); // Entrada de dado para estado.
+    fgets(estado_2, 40, stdin); // Entrada de dado para estado.
 
     printf("Nome da cidade: ");
-    fgets(cidadeNome, 40, stdin); // Entrada de dado para cidade.
+    fgets(cidadeNome_2, 40, stdin); // Entrada de dado para cidade.
 
     printf("Digite o código da carta: ");
-    scanf("%s", &cartaCodigo);
+    scanf("%s", &cartaCodigo_2);
 
     printf("População: ");
-    scanf("%d", &populacao); // Entrada de dado para população.
+    scanf("%d", &populacao_2); // Entrada de dado para população.
 
     printf("Área em km²: ");
-    scanf("%f", &areaEmKM); // Entrada de dado para área em km.
+    scanf("%f", &areaEmKM_2); // Entrada de dado para área em km.
 
     printf("Digite o PIB: ");
-    scanf("%f", &pib); // Entrada de dado para PIB.
+    scanf("%f", &pib_2); // Entrada de dado para PIB.
 
     printf("Número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos); // Entrada de dado para pontos turísticos.
+    scanf("%d", &pontosTuristicos_2); // Entrada de dado para pontos turísticos.
 
     // Retirando o "\n" criado pela função fgets
-    estado[strcspn(estado, "\n")] = 0;
-    cidadeNome[strcspn(cidadeNome, "\n")] = 0;
+    estado_2[strcspn(estado_2, "\n")] = 0;
+    cidadeNome_2[strcspn(cidadeNome_2, "\n")] = 0;
+
+    // Calculando PIB per capita e Densidade Populacional da segunda carta.
+    float densidadePopulacional = (populacao / areaEmKM);
+    float densidadePopulacional_2 = (populacao_2 / areaEmKM_2);
 
     // Segunda impressão de dados.
     printf("Informações da carta número 2:\n");
-    printf("Estado: %s - Cidade: %s\n", estado, cidadeNome);
-    printf("Código: %s - População: %d\n", cartaCodigo, populacao);
-    printf("Área (km²): %.2f - PIB: %.2f\n", areaEmKM, pib);
-    printf("Em %s tem %d pontos turísticos.\n", cidadeNome, pontosTuristicos);
+    printf("Estado: %s - Cidade: %s\n", estado_2, cidadeNome_2);
+    printf("Código: %s - População: %d\n", cartaCodigo_2, populacao_2);
+    printf("Área (km²): %.2f - PIB: %.2f\n", areaEmKM_2, pib_2);
+    printf("Em %s tem %d pontos turísticos.\n", cidadeNome_2, pontosTuristicos_2);
+    printf("O PIB per capita: %.2f\n", pibPerCapita_2);
+    printf("Densidade Populacional: %.3f hab./km².\n", densidadePopulacional_2);
+
+    // Função para pausar a execução do código.
+    printf("Preciona ENTER para exibir a carta vencedora em relação a quantidade de pontos turisticos. \n");
+    getchar(); // getchar para ler a entrada padrão.
+    getchar(); // usado duas vezes para descartar o dado recebido.
+
+    if(pontosTuristicos > pontosTuristicos_2) {
+        printf("A Carta %s é a vencedora.\n", cartaCodigo);
+    } else {
+        printf("A Carta %s é a vencedora.\n", cartaCodigo_2);
+    }
 
     return 0; // Indica que o programa terminou a execução.
 
